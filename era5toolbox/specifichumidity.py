ed9de95931a7915e04dced00157c79dc4cd85478
simon <simon.moulds10@imperial.ac.uk>
2020-08-19 09:35:25 +0000

    name nc variable in specific humidity calc

--- era5toolbox/specifichumidity.py
+++ era5toolbox/specifichumidity.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 
 import os
-import tempfile
-import subprocess
 import numpy as np
 import xarray as xr
 import metpy.calc as mpcalc
+
 
 class SpecificHumidityERA5(object):
     def __init__(self, config):
@@ -15,7 +14,7 @@ class SpecificHumidityERA5(object):
         for year in self.config.years:
             for month in self.config.months:
                 
-                # construct filenames of dewpoint temperature and surface pressure
+                # Construct filenames of dewpoint temperature and surface pressure
                 d2mfn = os.path.join(
                     self.config.regrid_directory,
                     'era5_reanalysis_2m_dewpoint_temperature_' \
@@ -29,7 +28,7 @@ class SpecificHumidityERA5(object):
                     + year + month + '.nc'
                 )
                 
-                # construct filename of output netCDF
+                # Construct filename of output netCDF
                 qfn = os.path.join(
                     self.config.regrid_directory,
                     'era5_reanalysis_specific_humidity_' \
@@ -37,7 +36,7 @@ class SpecificHumidityERA5(object):
                     + year + month + '.nc'
                 )
 
-                # perform calculation using MetPy (this should take care of units automatically)
+                # Perform calculation using MetPy (this should take care of units automatically)
                 d2m = xr.open_dataset(d2mfn)
                 sp = xr.open_dataset(spfn)
                 q = mpcalc.specific_humidity_from_dewpoint(
@@ -46,8 +45,14 @@ class SpecificHumidityERA5(object):
                 )
                 qxr = xr.DataArray(
                     np.array(q),
-                    dims=d2m.d2m.dims,
                     coords=d2m.d2m.coords,
+                    dims=d2m.d2m.dims,
+                    name='q',
                     attrs={'units' : '1'}
                 )
                 qxr.to_netcdf(qfn)
+                
+                # Clean up
+                sp.close()
+                d2m.close()
+                qxr.close()
